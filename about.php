e761edcb39f85145cace6d20465e67ecd7f6860b
Amit Kushwaha <akkushwaha018@gmail.com>
2024-09-09 19:59:49 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,54 @@
+<?php include_once('./include/header.php') ?>
+
+<!-- hero section -->
+<img src="./assets/img/banner.webp" alt="Hero Banner" class="img-fluid pe-none" width="100%" />
+
+<section>
+    <div class="container">
+        <div class="row gy-4 align-items-center">
+            <div class="col-md-6">
+                <h2 class="text-primary">About Lucknow Escorts Service</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
+            </div>
+            <div class="col-md-6 col-lg-5 offset-lg-1">
+                <img src="./assets/img/top-rt-img.webp" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="about-para bg-body-secondary">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>About Lucknow Escorts Service</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row gy-4 align-items-center">
+            <div class="col-md-6 col-lg-5 offset-lg-1">
+                <img src="./assets/img/top-rt-img.webp" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h2 class="text-primary">About Lucknow Escorts Service</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
---
+++ profile.php
@@ -0,0 +1,167 @@
+<?php include_once('include/header.php'); ?>
+
+<!-- hero section -->
+<img src="./assets/img/banner.webp" alt="Hero Banner" class="img-fluid pe-none" width="100%" />
+
+<div class="container">
+    <div class="col-md-12">
+        <div class=" call-info-btn">
+            <a class="thm-btn"><span><img src="https://in.nsibal.com/imgs/call-icon.svg" alt="call icon"></span><span id="spanPhone">Phone</span></a>
+            <a class="thm-btn whatsapp-btn" href="#" target="_blank"><span><img src="https://in.nsibal.com/imgs/whatsapp.svg" alt="whatsapp"></span> Chat</a>
+        </div>
+    </div>
+</div>
+
+
+<section>
+    <div class="container">
+        <div class="row gy-4">
+            <div class="col-12">
+                <h2 class="text-center">VIP TODAY LOW PRICE 100% SECURE GENUINE CALL GIRL AFFORDABLE PRICE CALL NOW 247 AVAILABLE</h2>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-6">
+                <div class="profile-gallery">
+                    <ul>
+                        <li>
+                            <img src="assets/img/profile.jpeg" class="img-fluid">
+                        </li>
+                        <li>
+                            <img src="assets/img/profile.jpeg" class="img-fluid">
+                        </li>
+                        <li>
+                            <img src="assets/img/profile.jpeg" class="img-fluid">
+                        </li>
+                        <li>
+                            <img src="assets/img/profile.jpeg" class="img-fluid">
+                        </li>
+                        <li>
+                            <img src="assets/img/profile.jpeg" class="img-fluid">
+                        </li>
+                        <li>
+                            <img src="assets/img/profile.jpeg" class="img-fluid">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="profile-description mb-5">
+                    <h4 class="text-primary">Characteristics</h4>
+                    <div class="characteristics-girl-info ib-table">
+                        <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    Age
+                                </td>
+                                <td>
+                                    Yes
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Nationality
+                                </td>
+                                <td>
+                                    Yes
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Breast
+                                </td>
+                                <td>
+                                    Yes
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Body Type
+                                </td>
+                                <td>
+                                    Yes
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Gender
+                                </td>
+                                <td>
+                                    Yes
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Ethnicity
+                                </td>
+                                <td>
+                                    Yes
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Hair
+                                </td>
+                                <td>
+                                    Yes
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Shared
+                                </td>
+                                <td>
+                                    Yes
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="col-md-12 mt-4">
+                <div class="profile-description">
+                    <h4 class="text-primary">About me</h4>
+                    <p>
+                        ⭐⭐⭐⭐➡️ WHATSAPP MESSAGE ⬇️HOT & SEXY MODELS // COLLEGE GIRLS & AUNTIES AVAILABLE AVAILABLE FOR COMPLETE ENJOYMENT WITH HIGH PROFILE INDIAN MODEL AVAILABLE HOTEL & HOME SERVICE AVAILABLE ★ SAFE AND SECURE HIGH CLASS SERVICE AFFORDABLE RATE ★ 💯% SATISFACTION,UNLIMITED ENJOYMENT. ★ All Meetings are confidential and no information is provided to any one at any cost. ★ EXCLUSIVE PROFILes Are Safe and Consensual with Most Limits Respected ★ Service Available In: - HOME & HOTEL 24x7 AVAILABLE 3️⃣🌟5️⃣ 🌟6️⃣🌟Star Hotel Service .In Call & Out call SeRvIcEs : ★ A-Level (5 star escort) ★ Strip-tease ★ BBBJ (Bareback Blowjob)Receive advanced sexual techniques in different mode make their life more pleasurable. ★ Spending time in hotel rooms ★ BJ (Blowjob Without a Condom) ★ Completion (Oral to completion) ★ Covered (Covered blowjob Without condom 💯% SAFE AND SECURE GENUINE SERVICE UNLIMITED ENJOY KAVYA SERVICE COMPANY
+                    </p>
+                </div>
+            </div>
+            <hr>
+            <div class="col-md-12 mt-4 mb-4">
+                
+            </div>
+            <hr>
+            <div class="col-md-12">
+                <div class="service-callgirl">
+                <h4>Services</h4>
+                <div class="service-cta-list">
+                    <a href="#">
+                         French Kissing
+                     </a>
+                    <a href="#">
+                        Oral without condom
+                    </a>
+                    <a href="#">
+                        GFE (girl friend experience)
+                    </a>
+                    <a href="#">
+                        Group sex
+                    </a>
+                    <a href="#">
+                        Handjob
+                    </a>
+                    <a href="#">
+                        Video Call
+                    </a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include_once('include/footer.php'); ?>
