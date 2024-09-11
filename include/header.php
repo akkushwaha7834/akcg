747841396cdb25273c3ccb95c10e227c19740a65
amitfxcareers <amit@fxcareers.com>
2024-09-11 10:39:08 +0530

    Content updated

--- include/header.php
+++ include/header.php
@@ -41,7 +41,7 @@
                                 <a class="nav-link" href="./profile.php">Profile</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="./service.php">Service</a>
+                                <a class="nav-link" href="./gallery.php">Gallery</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="./rates.php">Rates</a>
