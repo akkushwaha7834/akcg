d4229af618257761e7e202c646f00c04e343b1a5
amitfxcareers <amit@fxcareers.com>
2024-09-11 17:27:23 +0530

    Gallery Pages created

--- gallery.php
+++ gallery.php
@@ -6,42 +6,7 @@
 <section>
     <div class="container">
         <div class="row gy-4 align-items-center">
-            <div class="col-md-6">
-                <h2 class="text-primary">About Lucknow Escorts Service</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
-            </div>
-            <div class="col-md-6 col-lg-5 offset-lg-1">
-                <img src="./assets/img/me.jpg" alt="" class="img-fluid">
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="about-para bg-body-secondary">
-    <div class="container">
-        <div class="row">
             <div class="col-md-12">
-                <h4>About Lucknow Escorts Service</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!
-                </p>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section>
-    <div class="container">
-        <div class="row gy-4 align-items-center">
-            <div class="col-md-6 col-lg-5 offset-lg-1">
-                <img src="./assets/img/me.jpg" alt="" class="img-fluid">
-            </div>
-            <div class="col-md-6">
                 <h2 class="text-primary">About Lucknow Escorts Service</h2>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
@@ -51,20 +16,91 @@
     </div>
 </section>
 
-<section class="about-para bg-body-secondary">
+<section class="pt-0">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h4>About Lucknow Escorts Service</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!
-                </p>
+                <ul class="list-unstyled city-lists gallery_image">
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/2.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/3.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/4.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/5.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/6.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/7.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/8.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/9.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/10.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/11.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/12.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/13.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                    <li>
+                        <img src="./assets/img/1.jpg" class="img-fluid">
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
 </section>
+
 
 <?php include_once('include/footer.php'); ?>
