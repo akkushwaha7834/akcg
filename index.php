79eb30ffd16a5c00ee3ce4530df291c8480fb7b5
amitfxcareers <amit@fxcareers.com>
2024-09-12 16:57:02 +0530

    Header Footer included

--- index.php
+++ index.php
@@ -1,4 +1,57 @@
-<?php include_once('include/header.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home Page</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./assets/css/style.css" />
+    <link rel="stylesheet" href="./assets/css/responsive.css" />
+    <link rel="stylesheet" href="./assets/font-awesome/css/all.min.css">
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-0 header" id="header">
+        <div class="container">
+            <a class="navbar-brand py-0" href="./">
+                <span class="h2 fw-semibold">LOGO</span>
+            </a>
+            <button class="navbar-toggler border-0" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end bgc-primary" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <div class="offcanvas-header">
+                    <a href="./">
+                        <span class="h2 fw-semibold">LOGO</span>
+                    </a>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <div class="navbar-collapse">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-lg-3">
+                            <li class="nav-item">
+                                <a class="nav-link" href="./">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./about.php">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./gallery.php">Gallery</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./rates.php">Rates</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="./contact.php">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 
 <section class="banner">
    <img src="./assets/img/banner-1.webp" class="img-fluid" width="100%">
@@ -823,4 +876,166 @@
       </div>
    </div>
 </section>
-<?php include_once('include/footer.php'); ?>
+
+<!-- footer start -->
+<footer class="bgc-primary">
+    <div class="container">
+        <div class="row gy-4">
+            <div class="col-lg-3">
+                <div class="h5 fw-medium text-decoration-underline text-white mb-3">DESCLAIMER</div>
+                <p class="mb-0">Make your nights in Indore unforgettable with the company of a top-tier Indore call girl. These call girls offer more than just beauty; they bring charm, sophistication, and a personalized touch to every encounter. </p>
+            </div>
+            <div class="col-lg-8 offset-lg-1">
+                <div class="row gy-4">
+                    <div class="col-md-4 col-6">
+                        <div class="h5 fw-medium text-decoration-underline text-white mb-3">QUICK LINKS</div>
+                        <ul class="list-unstyled">
+                            <li><a href="./">Home</a></li>
+                            <li><a href="./about.php">About</a></li>
+                            <li><a href="./gallery.php">Gallery</a></li>
+                            <li><a href="./rates.php">Rates</a></li>
+                            <li><a href="./contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4 col-6">
+                        <div class="h5 fw-medium text-decoration-underline text-white mb-3">TOP CITIES</div>
+                        <ul class="list-unstyled">
+                            <li><a href="#">Saket Nagar Escort</a></li>
+                            <li><a href="#">Vijay Nagar Escort</a></li>
+                            <li><a href="#">Palasia Escort</a></li>
+                            <li><a href="#">Bhawar Kuan Escort</a></li>
+                            <li><a href="#">Rajwada Escort</a></li>
+                            <li><a href="#">Pithampur Escort</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="h5 fw-medium text-decoration-underline text-white mb-3">CONTACT</div>
+                        <ul class="list-unstyled">
+                            <li class="mb-2">
+                                <a href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)" height="1em">
+                                        <path d="M21 16.42V19.9561C21 20.4811 20.5941 20.9167 20.0705 20.9537C19.6331 20.9846 19.2763 21 19 21C10.1634 21 3 13.8366 3 5C3 4.72371 3.01545 4.36687 3.04635 3.9295C3.08337 3.40588 3.51894 3 4.04386 3H7.5801C7.83678 3 8.05176 3.19442 8.07753 3.4498C8.10067 3.67907 8.12218 3.86314 8.14207 4.00202C8.34435 5.41472 8.75753 6.75936 9.3487 8.00303C9.44359 8.20265 9.38171 8.44159 9.20185 8.57006L7.04355 10.1118C8.35752 13.1811 10.8189 15.6425 13.8882 16.9565L15.4271 14.8019C15.5572 14.6199 15.799 14.5573 16.001 14.6532C17.2446 15.2439 18.5891 15.6566 20.0016 15.8584C20.1396 15.8782 20.3225 15.8995 20.5502 15.9225C20.8056 15.9483 21 16.1633 21 16.42Z"></path>
+                                    </svg>
+                                    <span>+91 9876543210</span>
+                                </a>
+                            </li>
+                            <li class="mb-2">
+                                <a href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)" height="1em">
+                                        <path d="M3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4C2 3.44772 2.44772 3 3 3ZM12.0606 11.6829L5.64722 6.2377L4.35278 7.7623L12.0731 14.3171L19.6544 7.75616L18.3456 6.24384L12.0606 11.6829Z"></path>
+                                    </svg>
+                                    <span>info@website.com</span>
+                                </a>
+                            </li>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)" height="1em">
+                                    <path d="M21 20H23V22H1V20H3V3C3 2.44772 3.44772 2 4 2H20C20.5523 2 21 2.44772 21 3V20ZM8 11V13H11V11H8ZM8 7V9H11V7H8ZM8 15V17H11V15H8ZM13 15V17H16V15H13ZM13 11V13H16V11H13ZM13 7V9H16V7H13Z"></path>
+                                </svg>
+                                <span>Indore, MP</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="row gy-3 align-items-center">
+            <div class="col-md-6">
+                <div class="d-flex align-items-center gap-2 gap-lg-3 flex-wrap justify-content-center justify-content-md-start">
+                    <div class="h5 fw-medium mb-0">Follow us:</div>
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-x-twitter"></i>
+                    </a>
+                    <a href="#" target="_blank">
+                        <i class="fa-brands fa-youtube"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-6 text-center text-md-end">
+                <p class="mb-0">Copyright &copy; 2024 All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- footer end -->
+
+
+<!-- modal popup to show model details -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-centered modal-dialog-scrollable modal-lg">
+        <div class="modal-content">
+            <div class="modal-header border-0">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Model Details</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row gy-4">
+                    <div class="col-md-6 text-center">
+                        <img src="./assets/img/me.jpg" alt="" class="object-fit-cover rounded" height="400">
+                    </div>
+                    <div class="col-md-6">
+                        <ul class="list-unstyled d-flex flex-column gap-2 model-pop-detail">
+                            <li class="d-flex gap-3 py-2 px-3 rounded">
+                                <strong class="text-primary">Name:</strong>
+                                <span>Awantika</span>
+                            </li>
+                            <li class="d-flex gap-3 py-2 px-3 rounded">
+                                <strong class="text-primary">Age:</strong>
+                                <span>21</span>
+                            </li>
+                            <li class="d-flex gap-3 py-2 px-3 rounded">
+                                <strong class="text-primary">Location:</strong>
+                                <span>Indore, New Indore</span>
+                            </li>
+                            <li class="d-flex gap-3 py-2 px-3 rounded">
+                                <strong class="text-primary">Figure:</strong>
+                                <span>32, 30, 44</span>
+                            </li>
+                            <li class="d-flex gap-3 py-2 px-3 rounded">
+                                <strong class="text-primary">Hair and Eyes:</strong>
+                                <span>Blonde</span>
+                            </li>
+                            <li class="d-flex gap-3 py-2 px-3 rounded">
+                                <strong class="text-primary">Height:</strong>
+                                <span>5'1 ft</span>
+                            </li>
+                            <li class="d-flex gap-3 py-2 px-3 rounded">
+                                <strong class="text-primary">Body weight:</strong>
+                                <span>54 kg</span>
+                            </li>
+                        </ul>
+                        <div class="pro-social">
+                            <span><a href="tel:+910000000000"><i class="fa-solid fa-phone"></i></a></span>
+                            <span><a href="http://wa.me/910000000000"><i class="fa-brands fa-whatsapp"></i></a></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="nowcalling">
+   <div class="lcol">
+      <a href="tel:+911234567890">
+         Call Now
+      </a>
+   </div>
+   <div class="rcol">
+      <a href="#">
+         WhatsApp
+      </a>
+   </div>
+</div>
+
+<script src="./assets/js/bootstrap.bundle.min.js"></script>
+<script src="./assets/js/script.js"></script>
+</body>
+
+</html>
